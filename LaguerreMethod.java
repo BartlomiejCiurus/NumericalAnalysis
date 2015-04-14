3aa7f16326a17cf933ebe88b29dd23a3e3df3104
BartekCiurus <bartek.ciurus@gmail.com>
2015-04-14 20:11:50 +0200

    JNumericalAnalysis

--- LaguerreMethod.java
+++ LaguerreMethod.java
@@ -6,22 +6,22 @@ import biz.source_code.dsp.math.PolynomialRootFinderLaguerre;
 public class LaguerreMethod {
 
     public static void main(String[] args) {
-        Complex[] podpunktA = {new Complex(243,0),new Complex(-
+        Complex[] arrayA = {new Complex(243,0),new Complex(-
                 486,0),new Complex(783,0),new Complex(-990,0),new Complex(558,0)
                 ,new Complex(-28,0),new Complex(-72,0),new
                 Complex(16,0)};
-        Complex[] podpunktB = {new Complex(1,0),new Complex(1,0),new
+        Complex[] arrayB = {new Complex(1,0),new Complex(1,0),new
                 Complex(3,0),new Complex(2,0),new Complex(-1,0)
                 ,new Complex(-3,0),new Complex(-11,0),new Complex(-
                 8,0),new Complex(-12,0),new Complex(-4,0),new Complex(-4,0)};
-        Complex[] podpunktC = {new Complex(1,0),new Complex(0,1),new
+        Complex[] arrayC = {new Complex(1,0),new Complex(0,1),new
                 Complex(-1,0),new Complex(0,-1),new Complex(1,0)};
         Complex[] resultA =
-                PolynomialRootFinderLaguerre.findRoots(podpunktA);
+                PolynomialRootFinderLaguerre.findRoots(arrayA);
         Complex[] resultB =
-                PolynomialRootFinderLaguerre.findRoots(podpunktB);
+                PolynomialRootFinderLaguerre.findRoots(arrayB);
         Complex[] resultC =
-                PolynomialRootFinderLaguerre.findRoots(podpunktC);
+                PolynomialRootFinderLaguerre.findRoots(arrayC);
         System.out.println("Miejsca zerowe pierwszego wielomianu:");
         for (Complex complex : resultA) {
             System.out.println(complex);
